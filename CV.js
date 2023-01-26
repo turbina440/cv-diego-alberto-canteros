71aa4d810d6c3ca8e17c2479c75af61a3342f396
Diego Canteros <turbinagalaxy@gmail.com>
2023-01-26 13:55:34 -0300

    insertar el archivo

---
+++ CV.js
@@ -0,0 +1,17 @@
+/*document.getElementById('ver-cv').onclick= function () {
+console.log("mirando el cv de diego");
+//document.body.style.color="red";
+//}
+
+document.main.style.background="red";}
+
+*/
+/*document.getElementById('ver-cv').addEventListener('click',function(){
+document.getElementById('forro').style.display='none';
+console.log("sigo probando");
+});
+*/
+document.getElementById('tapar-cv').addEventListener('click',function(){
+document.getElementById('forro').style.display='block';
+console.log("sigo probando");
+});
