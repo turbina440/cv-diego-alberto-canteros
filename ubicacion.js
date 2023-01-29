0f7aa2fb3314dcffa7f86339a62ff1f571387796
Diego Canteros <123302060+turbina440@users.noreply.github.com>
2023-01-29 00:37:10 -0300

    Update ubicacion.js

--- ubicacion.js
+++ ubicacion.js
@@ -1,16 +1,3 @@
-/*function myMap() {
-  var coord = {lat:-34.68818 ,lng: -58.56000};
-var map = new google.maps.Map(document.getElementById("map"),{   
-zoom:17, 
- mapTypeId: google.maps.MapTypeId.ROADMAP,
-center:coord
- });
-var marker=new google.maps.Marker({
- position:coord,
- map: map
- });
-
-}*/
 
 
 function myMap() {
