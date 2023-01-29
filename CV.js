cedbbbb458ab4a0a94df6aaaf1fa794ce3ecdc15
Diego Canteros <123302060+turbina440@users.noreply.github.com>
2023-01-29 00:40:30 -0300

    Update CV.js

--- CV.js
+++ CV.js
@@ -1,16 +1,4 @@
-/*document.getElementById('ver-cv').onclick= function () {
-console.log("mirando el cv de diego");
-//document.body.style.color="red";
-//}
 
-document.main.style.background="red";}
-
-*/
-/*document.getElementById('ver-cv').addEventListener('click',function(){
-document.getElementById('forro').style.display='none';
-console.log("sigo probando");
-});
-*/
 document.getElementById('mostrar-cv').addEventListener('click',function(){
 document.getElementById('main').style.display='block';
 document.getElementById('ocultar-footer').style.display='block';
